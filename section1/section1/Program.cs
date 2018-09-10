8a8d4ea6fdb9388c54775e142f921847554146ba
axel.bendo <axel.bendo@NEITD-123420>
2018-09-10 18:36:26 -0500

    more experience

--- section1/section1/Program.cs
+++ section1/section1/Program.cs
@@ -4,92 +4,109 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace section1
+namespace Section1
 {
     class Program
     {
-        private static int minValue;
-
         static void Main( string[] args )
         {
-
             bool notQuit;
             do
             {
-               notQuit = DisplayMenu();
+                notQuit = DisplayMenu();
             } while (notQuit);
 
-
-
-            //playWithStrings();
-
+            //PlayWithStrings();
         }
 
-        private static void playWithStrings()
+        private static void PlayWithStrings()
         {
             string hoursString = "10A";
+
+            //From string
             //int hours = Int32.Parse(hoursString);
             //int hours;
-            //bool result = Int32.TryParse(hoursString,out  hours);
-            //bool result = Int32.TryParse(hoursString, out int  hours);
+            //bool result = Int32.TryParse(hoursString, out hours);
+            //bool result = Int32.TryParse(hoursString, out int hours);
 
-            // hoursString = hours.ToString();
-            // 4.75.ToString();
+            //To string
+            //hoursString = hours.ToString();
+            //4.75.ToString();
             //457.ToString();
             //Console.ReadLine().ToString();
 
             string message = "Hello\tworld";
             string filePath = "C:\\Temp\\Test";
 
-            //Verbatim strings 
+            //Verbatim strings
             filePath = @"C:\Temp\Test";
 
             //Concat
-            string FirstName = "Bob";
+            string firstName = "Bob";
             string lastName = "Smith";
-            string name = FirstName + "  " + lastName;
+            string name = firstName + " " + lastName;
 
-            //strings are immutable - this produces a new string 
+            //Strings are immutable - this produces a new string
             //name = "Hello " + name;
-            Console.WriteLine("Hello" + name);
-            Console.WriteLine("Hello {1}", FirstName, lastName);
-            string str = String.Format("Hello {1}", FirstName, lastName);
+            Console.WriteLine("Hello " + name);  // Approach 1
+            Console.WriteLine("Hello {0} {1}", firstName, lastName); //Approach 2
+            string str = String.Format("Hello {0} {1}", firstName, lastName); //Approach 3
             Console.WriteLine(str);
 
-
             //Approach 4
-            Console.WriteLine($"Hello  {FirstName} {lastName} ");
+            Console.WriteLine($"Hello {firstName} {lastName}");
 
+            //Null vs empty
             string missing = null;
             string empty = "";
             string empty2 = String.Empty;
 
             //Checking for empty strings
-            //if (firstName.Lenght == 0)
-            // if (FirstName!= null && FirstName != "")
-            if (!String.IsNullOrEmpty(FirstName))
-                Console.WriteLine(FirstName);
+            //if (firstName.Length == 0)
+            //if (firstName != null && firstName != "")
+            if (!String.IsNullOrEmpty(firstName))
+                Console.WriteLine(firstName);
 
             //Other stuff
-            string upperName = FirstName.ToUpper();
-            string lowerName = FirstName.ToLower();
+            string upperName = firstName.ToUpper();
+            string lowerName = firstName.ToLower();
 
             //Comparison
-            bool areEqual = FirstName == lastName;
+            bool areEqual = firstName == lastName;
+            areEqual = firstName.ToLower() == lastName.ToLower();
+            areEqual = String.Compare(firstName, lastName, true) == 0;
 
-            areEqual = FirstName.ToLower() == lastName.ToLower();
-            areEqual = String.Compare(FirstName, lastName, true) == 0;
-
-            bool startsWithA = FirstName.StartsWith("A");
-            bool endsWithA = FirstName.EndsWith("A");
-            bool hasA = FirstName.IndexOf("A") >= 0;
-            String subset = FirstName.Substring(4);
+            bool startsWithA = firstName.StartsWith("A");
+            bool endsWithA = firstName.EndsWith("A");
+            bool hasA = firstName.IndexOf("A") >= 0;
+            string subset = firstName.Substring(4);
 
             //Clean up
-            string cleanMe = FirstName.Trim(); //trimstart, TrimEnd
-            string makeLonger = FirstName.PadLeft(20); //PadRight
+            string cleanMe = firstName.Trim(); //TrimStart, TrimEnd
+            string makeLonger = firstName.PadLeft(20); //PadRight
+        }
 
+        private static void PlayWithArrays()
+        {
+            int count = ReadInt32("How many names? ", 1);
 
+            string[] names = new string[count];
+            for (int index = 0; index < count; ++index)
+            {
+                Console.WriteLine("Name? ");
+                names[index] = Console.ReadLine();
+            };
+
+            foreach (string name in names)
+            //for (int index = 0; index < names.Length; ++index)
+            {
+                //readonly - not allowed
+                //name = "";
+                string str = name;
+                str = "";
+                //Console.WriteLine(names[index]);
+                Console.WriteLine(name);
+            };
         }
 
         private static bool DisplayMenu()
@@ -99,65 +116,41 @@ namespace section1
                 Console.WriteLine("A)dd Movie");
                 Console.WriteLine("E)dit Movie");
                 Console.WriteLine("D)elete Movie");
-                Console.WriteLine("v)iew Movies");
+                Console.WriteLine("V)iew Movies");
                 Console.WriteLine("Q)uit");
-
-
 
                 string input = Console.ReadLine();
                 switch (input[0])
-
-
-
-
-
                 {
-
-
                     case 'a':
                     case 'A':
                     AddMovie();
                     return true;
+
                     case 'e':
                     case 'E':
                     EditMovie();
                     return true;
+
                     case 'd':
                     case 'D':
                     DeleteMovie();
                     return true;
+
                     case 'v':
                     case 'V':
                     ViewMovies();
                     return true;
+
                     case 'q':
                     case 'Q':
-                    ;
                     return false;
-
 
                     default:
                     Console.WriteLine("Please enter a valid value.");
                     break;
                 };
-
             };
-        }
-
-        private static void ViewMovies()
-        {
-            Console.WriteLine("ViewMovies");
-        }
-
-        private static void AddMovie()
-        {
-            Console.WriteLine("AddMovie");
-        }
-
-
-        private static void EditMovie()
-        {
-            Console.WriteLine("EditMovie");
         }
 
         private static void DeleteMovie()
@@ -165,9 +158,24 @@ namespace section1
             Console.WriteLine("DeleteMovie");
         }
 
-        private static int ReadInt32( string message )
+        private static void ViewMovies()
+        {
+            Console.WriteLine("ViewMovies");
+        }
 
+        private static void EditMovie()
+        {
+            Console.WriteLine("EditMovie");
+        }
 
+        private static void AddMovie()
+        {
+            name = ReadString("Enter a name: ", true);
+            description = ReadString("Enter a description: ");
+            runLength = ReadInt32("Enter run length (in minutes): ", 0);
+        }
+
+        private static int ReadInt32( string message, int minValue )
         {
             while (true)
             {
@@ -183,6 +191,30 @@ namespace section1
                 Console.WriteLine($"You must enter an integer value >= {minValue}");
             };
         }
-    }
 
+        private static string ReadString( string message )
+        {
+            return ReadString(message, false);
+        }
+
+        private static string ReadString( string message, bool required )
+        {
+            while (true)
+            {
+                Console.WriteLine(message);
+                string input = Console.ReadLine();
+
+                if (!String.IsNullOrEmpty(input) || !required)
+                    return input;
+
+                Console.WriteLine("You must enter a value");
+            };
+        }
+
+        //A movie
+        static string name;
+        static string description;
+        static int runLength;
+        //static DateTime releaseDate;
+    }
 }
