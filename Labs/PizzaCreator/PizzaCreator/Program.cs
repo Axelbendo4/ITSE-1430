3418c3d288cb81a324e4af2ae8f3b12a467df079
axel.bendo <axel.bendo@NEITD-123933>
2018-09-11 20:38:04 -0500

    PizzaCreator lab 1

--- Labs/PizzaCreator/PizzaCreator/Program.cs
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -8,8 +8,85 @@ namespace PizzaCreator
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            bool notQuit;
+            do
+
+
+            {
+                notQuit = DisplayMenu();
+
+            } while (notQuit);
+        }
+        private static bool DisplayMenu()
+        {
+
+            while (true)
+            {
+                Console.WriteLine("N)ew Order");
+                Console.WriteLine("M)odify Order");
+                Console.WriteLine("D)isplay Order");
+                Console.WriteLine("Q)uit");
+
+                string input = Console.ReadLine();
+                switch (input[0])
+                {
+                    case 'n':
+                    case 'N':
+                        NewOrder();
+                        return true;
+
+                    case 'm':
+                    case 'M':
+                        ModifyOrder();
+                        return true;
+
+                    case 'd':
+                    case 'D':
+                        DisplayOrder();
+                        return true;
+
+
+
+                    case 'q':
+                    case 'Q':
+                        Quit();
+                            return false;
+
+                    default:
+
+                        Console.WriteLine("Please enter a valid value");
+                        break;
+                };
+            };
+        }
+
+        private static void Quit()
+        {
+            Console.WriteLine("Quit");
+        }
+
+        private static void DisplayOrder()
+        {
+            Console.WriteLine("DisplayOrder");
+        }
+
+        private static void ModifyOrder()
+        {
+            Console.WriteLine("ModifyOrder");
+        }
+
+        private static void NewOrder()
+        {
+           
         }
     }
 }
+
+
+
+
+  
+
