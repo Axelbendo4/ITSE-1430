bd2299654200672a9cc06f58bb20644895a0446a
axel.bendo <axel.bendo@NEITD-123420>
2018-10-01 19:02:42 -0500

    more

--- Labs/CharacterCreator.Winforms/CharacterCreator.Winforms/MainForm.cs
+++ Labs/CharacterCreator.Winforms/CharacterCreator.Winforms/MainForm.cs
@@ -10,11 +10,14 @@ using System.Windows.Forms;
 
 namespace CharacterCreator.Winforms
 {
-    public partial class Form1 : Form
+    public partial class    MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
+
+        
+        
     }
 }
