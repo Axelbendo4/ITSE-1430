16be199368ebd015dd3bf8cfe809d83a271694ad
axel.bendo <axel.bendo@NEITD-123940>
2018-10-04 17:08:35 -0500

    more

---
+++ Labs/CharacterCreator.Winforms/CharacterCreator.UI/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterCreator.Winforms
+{
+    public partial class CharacterForm : Form
+    {
+        public CharacterForm()
+        {
+           
+        }
+    }
+}
