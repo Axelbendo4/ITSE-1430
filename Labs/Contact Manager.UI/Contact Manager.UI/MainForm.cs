69d3838692f5891a9ac24f3ede717947911f6b65
axel.bendo <axel.bendo@NEITD-123420>
2018-10-29 17:34:21 -0500

    story 1 done

---
+++ Labs/Contact Manager.UI/Contact Manager.UI/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact_Manager.UI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnFileExit_Click( object sender, EventArgs e )
+        {
+            if (MessageBox.Show("Do you want to exit ?", "Close", MessageBoxButtons.YesNo) == DialogResult.No)
+                return;
+            Close();
+        }
+
+        private void OnHelpAbout_Click( object sender, EventArgs e )
+        {
+            MessageBox.Show(this, "Axel Gaucen Bendo\n ITSE 1430\n  Contact Manager ", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    
+    }
+}
