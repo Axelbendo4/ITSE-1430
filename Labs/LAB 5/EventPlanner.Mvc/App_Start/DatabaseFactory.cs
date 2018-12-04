537b6f806792dbfbb684288a9a0001c633a15286
axel.bendo <axel.bendo@NEITD-123925>
2018-12-04 15:30:29 -0600

    lab 5 no excse

---
+++ Labs/LAB 5/EventPlanner.Mvc/App_Start/DatabaseFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventPlanner.Mvc.App_Start
+{
+    public static  class DatabaseFactory
+    {
+        private static IEventDatabase _databse = new MemoryEventDatabase().SeedDatabase();
+
+        private static MemoryEventDatabase SeedDatabase(this MemoryEventDatabase mmd)
+        {
+            mmd.AddRange(new List<ScheduledEvent>{
+                new ScheduledEvent(){
+                    Description = "",
+                    Id = 1,
+                    Name = "Start Event",
+                    StartDate = new DateTime(2018, 4, 30),
+                    EndDate = DateTime.Today,
+                    IsPublic = true},
+                new ScheduledEvent(){
+                    Description = "Celebrating love",
+                    Id = 1,
+                    Name = "Wedding",
+                    StartDate = new DateTime(1991, 4, 30),
+                    EndDate = new DateTime(1991, 4, 30),
+                    IsPublic = false},
+        });
+
+            return mmd;
+        }
+
+        private static List<ScheduledEvent> AddRange(this MemoryEventDatabase mmd, List<ScheduledEvent> evts)
+        {
+            List<ScheduledEvent> rtnEvents = new List<ScheduledEvent>();
+
+            evts.ForEach(evt => rtnEvents.Add(mmd.Add(evt)));
+
+            return rtnEvents;
+        }
+
+        public static IEventDatabase Database
+        {
+            get
+            { return _databse; }
+            private set { _databse = value; }
+        }
+
+
+
+    }
+}
